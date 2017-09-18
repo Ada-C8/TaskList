b41afb9d8af156d7082982dea8320f992a842694
murog <guillermina.muro@gmail.com>
2017-09-18 15:36:46 -0700

    updated controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,26 @@
 class TasksController < ApplicationController
+TASKS = [
+  {id: 1,
+  title:"pay car insurance",
+  status:"incomplete"
+},
+{id: 2,
+title:"laundry",
+status:"incomplete"},
+{id: 3,
+title:"buy groceries",
+status:"incomplete",
+description:"spinach, maybe new bread"}
+]
+
   def index
+    id = params[:id].to_i
+    @task = nil
+    TASKS.each do |task|
+      if task[:id] == id
+        @task = task
+      end
+    end  
   end
 
   def show
