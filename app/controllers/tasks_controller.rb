c7cab693df08773c2796ada98e29ffc10e841cde
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-09-18 16:54:47 -0700

    Baseline complete.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,26 @@
+class TasksController < ApplicationController
+  TASKS = [
+    {id: 1, description: "Feed Harriet", due_date: "Wednesday"},
+    {id: 2, description: "Call Car 2 Go", due_date: "Friday"},
+    {id: 3, description: "Finish TaskList", due_date: "10/1/17"}
+  ]
+
+  def index
+    @tasks = TASKS
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destory
+  end
+end
