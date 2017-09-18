4c393e68bbf5029c47b568594bdd7a99d9dc5624
Amy Lee <amy.yj.lee@gmail.com>
2017-09-18 15:27:55 -0700

    set up routes and generated a basic template for controller

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get '/tasks/', to: 'tasks#index', as: 'tasks' #tasks_path
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' #edit_task_path
+
+  get '/tasks/new', to: 'tasks#new', as: 'new-task' #new_task_path
+
+  get '/tasks/:id', to: 'tasks#show', as: 'task' #task_path
+
+  patch '/tasks/:id', to: 'tasks#update', as: 'update_task' #update_task_path
+
+  post '/tasks', to: 'tasks#ceate', as: 'create_task' #create_task_path
+
+  delete '/tasks', to: 'tasks#destroy', as: 'delete_task' #delete_task_path
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
