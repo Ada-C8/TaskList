f0339090fc6608be7f1c7a6b5369fee2f370843b
Jessica Owens <vertige@gmail.com>
2017-09-18 16:13:35 -0700

    Initial setup of TasList Rails project, Tasks controller, and filled routes

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  #get 'tasks/index'
+  get 'tasks', to: 'tasks#index', as: 'tasks' #=> tasks_path, displays all tasks
+
+  #get 'tasks/edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' #=> edit_task_path
+
+  #get 'tasks/new'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task' #=> new_task_path
+
+  #get 'tasks/show'
+  get 'tasks/:id', to: 'tasks#show', as: 'book' #=> book_path
+
+  #get 'tasks/update'
+  patch 'tasks/:id', to: 'tasks#update', as: 'update_book' #=> update_book_path
+
+  #get 'tasks/create'
+  post 'tasks', to: 'tasks#create', as: 'create_book' #=> create_book_path
+
+  #get 'tasks/destroy'
+  delete 'tasks', to: 'tasks#destroy', as: 'delete_book' #=> delete_book_path
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
