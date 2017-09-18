b85aa0b96a473886bc8a88e1c0eb469504626081
Sarah Read-Brown <srbusiness2@gmail.com>
2017-09-18 15:01:10 -0700

    added hardcoded tasks to tasks controller

--- app/controllers/task_list_controller.rb
+++ app/controllers/task_list_controller.rb
@@ -1,4 +1,9 @@
 class TaskListController < ApplicationController
   def index
+    @tasks = [{ title: "Shop for food", description: "Go to the grocery store and buy indredients for a pasta bake", due_date: "9/20/2017", completion_status: "incomplete"},
+      { title: "Hang white board", description: "Install hanger clips and hang white board in living room", due_date: "9/23/2017", completion_status: "incomplete"},
+      { title: "Renew passport", description: "Get headshots taken, fill out paperwork, mail into us gov office", due_date: "10/1/2017", completion_status:  "incomplete"},
+      { title: "Call mom", description: "Call your mother!", due_date: "9/18/2017", completion_status:  "incomplete"}
+    ]
   end
 end
