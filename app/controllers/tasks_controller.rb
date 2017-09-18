c066f5c8a1286bce03f51daa6f53df539ab7cd99
enigmagnetic <lauren.cardella@gmail.com>
2017-09-18 16:35:58 -0700

    Set up project folder, define routes.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,22 @@
+class TasksController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
