a6be6ff8f45a8dc33b4acd1df535e4fa2914c798
Tanja Stroble <plusdeltah@gmail.com>
2017-09-18 16:40:43 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
 
-  get 'tasks/show'
+  get '/tasks', to: 'tasks_index', as: 'tasks' #tasks_path
 
-  get 'tasks/new'
+  get '/tasks/:title/edit', to: 'tasks#edit', as: 'edit_task' #edit_task_path
 
-  get 'tasks/create'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task' #new_task_path
 
-  get 'tasks/complete'
+  get 'tasks/:title', to: 'tasks#show', as: 'task' #task_path
 
-  get 'tasks/edit'
+  patch '/tasks:title', to:'tasks#update', as: 'update_task' #update_task_path
 
-  get 'tasks/update'
+  post '/tasks', to:'tasks#create', as: 'create_task' #create_task_path
 
-  get 'tasks/destroy'
+  patch '/tasks:completed', to: 'tasks#complete', as: 'complete_task' #complete_task_path
+
+  delete '/tasks', to: 'tasks#destroy', as: 'delete_task' #delete_task_path
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
