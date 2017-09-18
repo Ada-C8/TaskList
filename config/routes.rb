67de3e2c31333e034658b555d42d0359103644c4
Angela Wilson <wilsonange@gmail.com>
2017-09-18 15:29:09 -0700

    Create new rails server. Create new tasks controller. Router file, create some routes.

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+get "/tasks/new", to: "tasks#new"
+post "/tasks", to: "tasks#create"
+
+get "/tasks/:id/edit", to: "tasks#edit"
+
+get "tasks/:id", to: "tasks#show"
+
+patch "/tasks/:id", to: "tasks#update"
+
+get "/tasks", to: "taks#index"
+
+delete "/tasks/:id", to: "tasks#destroy"
+
+
+end
