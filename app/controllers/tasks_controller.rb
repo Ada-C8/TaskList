acd47bd37042d8079e53e86017c281552e076836
kee nam <nam.keeyeon@gmail.com>
2017-09-18 14:50:29 -0700

    Added to router.rb, tasks_controller.rb, and an index.html.erb file

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,11 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = [
+      {name: "Do laundry", created: "2017/09/19", status: "Incomplete", due: "2017/09/21" },
+      {name: "Cook dinner", created: "2017/09/18", status: "Incomplete", due: "2017/09/18" },
+      {name: "Do homework", created: "2017/09/19", status: "Incomplete", due: "2017/09/19" },
+      {name: "Mail forms", created: "2017/09/19", status: "Complete", due: "2017/10/13" },
+      {name: "Text Kimmy", created: "2017/09/19", status: "Incomplete", due: "2017/09/19" }
+    ]
+  end
+end
