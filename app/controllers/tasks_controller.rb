ad609acdcee7a0cdde35ebdec765a17f90b7a2d3
Irene DeVera <irenneeyyy@gmail.com>
2017-09-19 15:26:56 -0700

    Create first migration and Task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,12 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ["Dishes", "Laundry", "Study"]
+    @tasks = [
+      { title: "Dishes", description: "I need to do the dishes today", date_due: "20SEP2017", status: false
+      },
+      { title: "Study", description: "Every day is a day to study programming!", date_due: "20SEP2017", status: false
+      },
+      { title: "Shopping", description: "I have no food in my fridge", date_due: "20SEP2017", status: false
+      }
+    ]
   end
 end
