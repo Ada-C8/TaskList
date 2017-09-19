bef2ac954b56a5818bcab721fd8386047b5ec933
Tamira Vojnar <tamiravojnar@gmail.com>
2017-09-19 16:43:59 -0700

    Completed wave 1

--- task-list/app/controllers/tasks_controller.rb
+++ task-list/app/controllers/tasks_controller.rb
@@ -1,9 +1,11 @@
 class TasksController < ApplicationController
   def index
-    @tasks = [
-      {title: "Clean kitchen", discription: "Clean the counters and the stove", due_date: "9/30/17", competion_status: "Not completed"},
-      {title: "Email Jeff", discription: "About meeting for coffee", due_date: "9/25/17", competion_status: "Not completed"},
-      {title: "Order shoes", discription: "Buy new sneakers for fall", due_date: "9/22/17", competion_status: "Completed"}
-    ]
+
+    @tasks = Task.all
+    # @tasks = [
+    #   {title: "Clean kitchen", discription: "Clean the counters and the stove", due_date: "9/30/17", competion_status: "Not completed"},
+    #   {title: "Email Jeff", discription: "About meeting for coffee", due_date: "9/25/17", competion_status: "Not completed"},
+    #   {title: "Order shoes", discription: "Buy new sneakers for fall", due_date: "9/22/17", competion_status: "Completed"}
+    # ]
   end # index
 end
