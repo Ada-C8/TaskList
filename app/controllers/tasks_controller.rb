10177f12dace0b1234eeae81d17979b850ff12ef
hellomarisamorris <hellomarisamorris@gmail.com>
2017-09-19 16:43:51 -0700

    Wave 1 commplete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,28 @@
 class TasksController < ApplicationController
 
-  TASKS = @tasks = [
-    {id: 1, task: "Re-learn to snowboard"},
-    {id: 2, task: "fold clothing"},
-    {id: 3, task: "walk the dogs"},
-    {id: 4, task: "re-read lecture and take notes"},
-    {id: 5, task: "clean room"},
-  ]
+  # TASKS = @tasks = [
+  #   {id: 1, task: "Re-learn to snowboard"},
+  #   {id: 2, task: "fold clothing"},
+  #   {id: 3, task: "walk the dogs"},
+  #   {id: 4, task: "re-read lecture and take notes"},
+  #   {id: 5, task: "clean room"},
+  # ]
 
   def index
-    @tasks = TASKS
+    # @tasks = TASKS
+    @tasks = Task.order(:id)
   end
 
   def show
-    id = params[:id].to_i
-    @task = nil
-
-    TASKS.each do |task|
-      if task[:id] == id
-        @task = task
-      end
-    end
+    @task = Task.find( params[:id].to_i)
+    # id = params[:id].to_i
+    # @task = nil
+    #
+    # @tasks.each do |task|
+    #   if task[:id] == id
+    #     @task = task
+    #   end
+    # end
   end
 
   def edit
