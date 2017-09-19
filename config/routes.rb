6a3e85737ed87025e6558fdcbc19ee5ee2ff8fc9
enigmagnetic <lauren.cardella@gmail.com>
2017-09-18 23:40:26 -0700

    Add link buttons for mark complete, edit, delete, add task and all tasks.

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   get 'tasks/new', to: 'tasks#new', as: 'new_task' #new_task_path
 
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' # edit_book_path
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' # edit_task_path
 
   post '/tasks', to: 'tasks#create', as: 'create_task' # create_task_path
 
   get '/tasks/:id', to: 'tasks#show', as: 'task' # task_path
 
-  patch '/tasks/:id', to: 'task#update', as: 'update_task' # update_task_path
+  patch '/tasks/:id', to: 'tasks#update', as: 'update_task' # update_task_path
 
 
   delete '/tasks/:id', to: 'tasks#destroy', as: 'delete_task' # delete_task_path
