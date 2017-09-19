69b7caa02d788671cacd6cbc12f8aeda2fae5c92
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-09-19 16:32:32 -0700

    Wave 1 complete.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,17 @@
 class TasksController < ApplicationController
-  TASKS = [
-    {id: 1, description: "Feed Harriet", due_date: "Wednesday"},
-    {id: 2, description: "Call Car 2 Go", due_date: "Friday"},
-    {id: 3, description: "Finish TaskList", due_date: "10/1/17"}
-  ]
+  # TASKS = [
+  #   {id: 1, description: "Feed Harriet", due_date: "Wednesday"},
+  #   {id: 2, description: "Call Car 2 Go", due_date: "Friday"},
+  #   {id: 3, description: "Finish TaskList", due_date: "10/1/17"}
+  # ]
 
   def index
-    @tasks = TASKS
+    @tasks = Task.order(:completion_date)
   end
 
   def show
+    name = params[:name]
+    @task = Task.find_by(name)
   end
 
   def create
@@ -19,12 +21,18 @@ class TasksController < ApplicationController
   end
 
   def mark_complete
+    name = params[:name]
+    @task = Task.find_by(name)
   end
 
   def edit
+    name = params[:name]
+    @task = Task.find_by(name)
   end
 
   def destory
+    name = params[:name]
+    @task = Task.find_by(name)
   end
 
 end
