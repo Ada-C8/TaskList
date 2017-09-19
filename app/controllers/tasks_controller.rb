371cbc87c1b59e34b5435ca7857057348dd706a6
Bennett Rahn <rahnibennett@gmail.com>
2017-09-19 14:46:17 -0700

    set up as many connections as I know how to do.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,6 @@
 class TasksController < ApplicationController
   def index
-    # @tasks = [
-    #   {title: "Water the garden", description: "especially don't forget the basil in the corner", due_date: "Tuesday", complete: false, id:1},
-    #   {title: "Feed the cat", description: "food is in the drawer under the dishes", due_date: "Thursday", complete: false, id:2},
-    #   {title: "Take out the trash", description: "don't forget to sort the recyclables", due_date: "Friday", complete: false, id:2}
-    # ]
+
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
 
   # resources :tasks
   get '/tasks', to:'tasks#index', as:'tasks'
-  # post '/tasks', to:'tasks#create'
-  # get 'tasks/new', to:'tasks#new', as:'new_task'
+  post '/tasks', to:'tasks#create'
+  get 'tasks/new', to:'tasks#new', as:'new_task'
   get '/tasks/:id/edit', to:'tasks#edit', as:'edit_task'
-  # get '/tasks/:id', to:'tasks#show', to:'task'
-  # patch '/tasks/:id', to:'tasks#update'
-  # put '/tasks/:id', to:'tasks#update'
-  # delete '/tasks/:id', to: 'tasks#destroy'
+  get '/tasks/:id', to:'tasks#show', as:'task'
+  patch '/tasks/:id', to:'tasks#update'
+  put '/tasks/:id', to:'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 
 end
