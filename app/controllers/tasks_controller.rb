0a38936a49aca3ee717ccc853197e3e0a0b29354
Bennett Rahn <rahnibennett@gmail.com>
2017-09-19 12:21:18 -0700

    play with link_to

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,37 @@
 class TasksController < ApplicationController
   def index
     @tasks = [
-      {title: "Water the garden", description: "especially don't forget the basil in the corner", due_date: "Tuesday", complete: false},
-      {title: "Feed the cat", description: "food is in the drawer under the dishes", due_date: "Thursday", complete: false},
-      {title: "Take out the trash", description: "don't forget to sort the recyclables", due_date: "Friday", complete: false}
+      {title: "Water the garden", description: "especially don't forget the basil in the corner", due_date: "Tuesday", complete: false, id:1},
+      {title: "Feed the cat", description: "food is in the drawer under the dishes", due_date: "Thursday", complete: false, id:2},
+      {title: "Take out the trash", description: "don't forget to sort the recyclables", due_date: "Friday", complete: false, id:2}
     ]
+  end
 
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def update
+
+  end
+
+  def destroy
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
 
   # resources :tasks
   get '/tasks', to:'tasks#index', as:'tasks'
-  # POST   /tasks(.:format)          tasks#create
-  # GET    /tasks/new(.:format)      tasks#new new_task
-  # GET    /tasks/:id/edit(.:format) tasks#edit edit_task
-  # GET    /tasks/:id(.:format)      tasks#show task
-  # PATCH  /tasks/:id(.:format)      tasks#update
-  # PUT    /tasks/:id(.:format)      tasks#update
-  # DELETE /tasks/:id(.:format)      tasks#destroy
+  # post '/tasks', to:'tasks#create'
+  # get 'tasks/new', to:'tasks#new', as:'new_task'
+  get '/tasks/:id/edit', to:'tasks#edit', as:'edit_task'
+  # get '/tasks/:id', to:'tasks#show', to:'task'
+  # patch '/tasks/:id', to:'tasks#update'
+  # put '/tasks/:id', to:'tasks#update'
+  # delete '/tasks/:id', to: 'tasks#destroy'
 
 end
