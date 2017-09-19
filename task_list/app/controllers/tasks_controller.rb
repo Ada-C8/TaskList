80934c8f464b815228861c3aa438ffb41ffa1658
IsabelDePapel <isuchanek@gmail.com>
2017-09-19 08:55:29 -0700

    Completed baseline

--- task_list/app/controllers/tasks_controller.rb
+++ task_list/app/controllers/tasks_controller.rb
@@ -1,8 +1,15 @@
 class TasksController < ApplicationController
+
+  TASKS = [ { id: 1, task_name: 'buy groceries', complete: true },
+            { id: 2, task_name: 'wash car', complete: false },
+            { id: 3, task_name: 'do laundry', complete: false },
+            { id: 4, task_name: 'lightning talk', complete: false } ]
+
   def new
   end
 
   def index
+    @tasks = TASKS
   end
 
   def edit
@@ -18,5 +25,13 @@ class TasksController < ApplicationController
   end
 
   def show
+    id = params[:id].to_i
+    @task = nil
+
+    TASKS.each do |task|
+      if task[:id] == id
+        @task = task
+      end
+    end
   end
 end
--- task_list/config/routes.rb
+++ task_list/config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  get 'tasks/new'
+  get '/tasks', to: 'tasks#index', as: 'tasks' # tasks_path
 
-  get 'tasks/index'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' # edit_task_path
 
-  get 'tasks/edit'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task' #new_task_path
 
-  get 'tasks/update'
+  get '/tasks/:id', to: 'tasks#show', as: 'task' #task_path
 
-  get 'tasks/destroy'
+  patch '/tasks/:id', to: 'tasks#update', as: 'update_task' # update_task_path
 
-  get 'tasks/create'
+  delete '/tasks', to: 'tasks#delete', as: 'delete_task' # delete_task_path
 
-  get 'tasks/show'
+  post '/tasks', to: 'tasks#create', as: 'create_task' # create_task_path
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
