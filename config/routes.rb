2297fcae027562e6cbf8a1b9b77c6986bae4f383
Isaac Del Rosario <cecdelr@gmail.com>
2017-09-19 03:28:26 -0700

    Changed HTTP verbs of routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
 
   get '/tasks/:id', to: 'tasks#show', as: 'task'
 
-  get '/tasks/:id', to: 'tasks#update', as: 'update_task'
+  patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
 
-  get '/tasks', to: 'tasks#create', as: 'create_task'
+  post '/tasks', to: 'tasks#create', as: 'create_task'
 
-  get '/tasks', to: 'tasks#delete', as: 'delete_task'
+  delete '/tasks', to: 'tasks#delete', as: 'delete_task'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
