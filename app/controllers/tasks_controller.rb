2b1a85970d8884118e8eebaecb61a0483c108f3e
Bennett Rahn <rahnibennett@gmail.com>
2017-09-19 12:36:51 -0700

    add db and switch basic index logic to db instead of hash.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   def index
-    @tasks = [
-      {title: "Water the garden", description: "especially don't forget the basil in the corner", due_date: "Tuesday", complete: false, id:1},
-      {title: "Feed the cat", description: "food is in the drawer under the dishes", due_date: "Thursday", complete: false, id:2},
-      {title: "Take out the trash", description: "don't forget to sort the recyclables", due_date: "Friday", complete: false, id:2}
-    ]
+    # @tasks = [
+    #   {title: "Water the garden", description: "especially don't forget the basil in the corner", due_date: "Tuesday", complete: false, id:1},
+    #   {title: "Feed the cat", description: "food is in the drawer under the dishes", due_date: "Thursday", complete: false, id:2},
+    #   {title: "Take out the trash", description: "don't forget to sort the recyclables", due_date: "Friday", complete: false, id:2}
+    # ]
   end
 
   def create
@@ -20,10 +20,6 @@ class TasksController < ApplicationController
   end
 
   def show
-
-  end
-
-  def update
 
   end
 
