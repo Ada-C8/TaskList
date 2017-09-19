f680b49c066a6740f767030aaee59ef01cc73b98
Irene DeVera <irenneeyyy@gmail.com>
2017-09-18 20:13:15 -0700

    Make css changes for application

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,5 @@
 class TasksController < ApplicationController
   def index
-    @books = [{ title: "Hidden Figures", author: "Margot Lee Shetterly"},
-             { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-             { title: "Kindred", author: "Octavia E. Butler"}]
+    @all_tasks = ["Dishes", "Laundry", "Study"]
   end
 end
