5f52a0fd2163cb15aada1e8d43fa1418d18a3667
Julia <julia.meier@gmail.com>
2017-09-19 16:53:31 -0700

    completed wave 1

--- app/controllers/task_list_controller.rb
+++ app/controllers/task_list_controller.rb
@@ -1,23 +1,29 @@
 class TaskListController < ApplicationController
-  TASK_LIST = [
-    {id: 1, status: false, action: "Master Rails"},
-    {id: 2, status: false, action: "Learn how to cook palak paneer"},
-    {id: 3, status: false, action: "Laundry"},
-    {id: 4, status: false, action: "Go for a walk"}
-  ]
+  # TASK_LIST = [
+  #   {id: 1, status: false, action: "Master Rails"},
+  #   {id: 2, status: false, action: "Learn how to cook palak paneer"},
+  #   {id: 3, status: false, action: "Laundry"},
+  #   {id: 4, status: false, action: "Go for a walk"}
+  # ]
 
   def index
-    @task_list = TASK_LIST
+    # @task_list = TASK_LIST
+    @tasks = Task.order(:completion_date)
+    @tasks.sort_by { |task| Date.strptime(task.completion_date, "%m/%d/%Y") }
+
+    # @tasks = Task.order(Date.strptime(:completion_date, "%m/%d/%Y"))
   end
 
   def show
-    id = params[:id].to_i
-    @task = nil
-    TASK_LIST.each do |task|
-      if task[:id] == id
-        @task = task
-      end
-    end
+
+    @task = Task.find( params[:id].to_i )
+    # id = params[:id].to_i
+    # @task = nil
+    # TASK_LIST.each do |task|
+    #   if task[:id] == id
+    #     @task = task
+    #   end
+    # end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get '/task_list/:id', to: 'task_list#show', as: 'task' #task_path
 
+  patch '/task_list/:id/mark_complete', to: 'task_list#mark_complete', as: 'mark_complete' # mark_complete_path
+
   patch '/task_list/:id', to: 'task_list#update', as: 'update_task' #update_task_path
 
   post '/task_list', to: 'task_list#create', as: 'create_task' #create_task_path
