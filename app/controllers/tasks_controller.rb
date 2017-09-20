8aafc06bf5bc0ea8c5ac405ef0ec23873fc9d918
Irene DeVera <irenneeyyy@gmail.com>
2017-09-20 10:47:42 -0700

    Add new.html.erb for the creation of a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,24 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+  end
+
+  def create
+    # you would want to use Task.save
+    redirect_to('/tasks') # This will redirect to the index page because we are telling where the path should go. This should ultimately direc to the show route
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
