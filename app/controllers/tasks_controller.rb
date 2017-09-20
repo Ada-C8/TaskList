805924657b0bd683cc1dd038c3274d4bad1b08f6
Tanisha <tanishahampden@gmail.com>
2017-09-20 14:52:49 -0700

    add show action and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,9 +7,18 @@ class TasksController < ApplicationController
   end
 
   def create # has access to user data, from the form
-    task = Task.new(name: params[:name], description: params[:description], completion_date: params[:completion_date])
-    task.save
+    task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date])
 
+    # task.name = params[:task][:name]
+
+    task.save
     redirect_to('/tasks')
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
   end
 end
