6987e5bb2a1cb2f65f2c551eac5e7b520d28eade
Amy Cash <amymcash@gmail.com>
2017-09-20 13:30:54 -0700

    reorganize routes and methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,28 +6,31 @@ class TasksController < ApplicationController
   def new #only cares about showing the form
   end
 
-  def edit_task
-  end
-
   def create #hash access to user data, from the form,
-    a = Task.new(title: params[:title], description: params[:description], date: params[:date])
-    a.save
-    redirect_to('/tasks')
-    # puts params
     # x = Task.new(title: "Ada" , description: "it's a name")
-    # puts x
-    # x.save
-
+    task = Task.new(title: params[:title], description: params[:description], date: params[:date])
+    #calls the form, in the first example, you create a new entry by adding the info yourself
+    #in the second example you use user input as the values --
+    task.save #saves it
+    redirect_to('/tasks')
+    #return user to main page
   end
 
   def show
+    @task = Task.find(2)
+    
+    #@task = Task.find(params[:id])
+    # @task = Task.find_by(params[:id])
+    # title: params[:book_id]
   end
 
   def update
   end
 
-  def show
+  def edit_task
   end
 
+  def destroy
+  end
 
 end
