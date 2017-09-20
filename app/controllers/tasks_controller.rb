1b06475263c87c218367d072b8c8dd2747e9d499
Angela Wilson <wilsonange@gmail.com>
2017-09-20 14:33:38 -0700

    complete wave 1: made index controller action and hard coded the tasks. made rudimentary views interface.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,36 +1,39 @@
 class TasksController < ApplicationController
 
-    def new
-      render plain: "added 'new' task"
-    end
+  @tasks = ["complete the death star", "clean the bathroom", "do laundry", "have fun"]
 
-    def index
-      # render plain: "added 'index' task"
-      render plain: @tasks = ["complete the death star", "clean the bathroom", "do laundry", "have fun"]
+  def new
+    render plain: "added 'new' task"
+  end
 
-    end
+  def index
+    @tasks
+    # render plain: "added 'index' task"
+    # render plain: @tasks = ["complete the death star", "clean the bathroom", "do laundry", "have fun"]
 
-    def edit
-      render plain: "add 'edit' task"
-    end
+  end
 
-    def show
-      render plain: "add 'show' task"
-    end
+  def edit
+    render plain: "add 'edit' task"
+  end
 
-    def update
-      render plain: "add 'update' task"
-    end
+  def show
+    render plain: "add 'show' task"
+  end
 
-    def completed
-      render plain: "add 'completed' task"
-    end
+  def update
+    render plain: "add 'update' task"
+  end
 
-    def create
-      render plain: "add 'create' task"
-    end
+  def completed
+    render plain: "add 'completed' task"
+  end
 
-    def destroy
-      render plain: "add 'destroy' task"
-    end
+  def create
+    render plain: "add 'create' task"
+  end
+
+  def destroy
+    render plain: "add 'destroy' task"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get "/tasks", to: "tasks#index", as: "tasks"
+  get "/tasks/new", to: "tasks#new", as: "add"
+  post "/tasks", to: "tasks#create"
 
-get "/tasks/new", to: "tasks#new"
-post "/tasks", to: "tasks#create"
 
-get "/tasks/:id/edit", to: "tasks#edit"
-
-get "tasks/:id", to: "tasks#show"
-
-patch "/tasks/:id", to: "tasks#update"
-
-get "/tasks", to: "tasks#index"
-
-delete "/tasks/:id", to: "tasks#destroy"
+  get "tasks/:id", to: "tasks#show"
+  get "/tasks/:id/edit", to: "tasks#edit"
+  patch "/tasks/:id", to: "tasks#update"
+  delete "/tasks/:id", to: "tasks#destroy"
 
 
 end
