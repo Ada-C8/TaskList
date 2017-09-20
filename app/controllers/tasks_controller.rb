96841b30b705527e991357ab9f1bb886c0f45731
Sarah Read-Brown <srbusiness2@gmail.com>
2017-09-20 11:07:07 -0700

    Adds more form inputs, reorgs controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,28 @@
 class TasksController < ApplicationController
+
   def index
-    # @tasks = [{ title: "Shop for food", description: "Go to the grocery store and buy indredients for a pasta bake", due_date: "9/20/2017", completion_status: "incomplete"},
-    #   { title: "Hang white board", description: "Install hanger clips and hang white board in living room", due_date: "9/23/2017", completion_status: "incomplete"},
-    #   { title: "Renew passport", description: "Get headshots taken, fill out paperwork, mail into us gov office", due_date: "10/1/2017", completion_status:  "incomplete"},
-    #   { title: "Call mom", description: "Call your mother!", due_date: "9/18/2017", completion_status:  "incomplete"}
-    # ]
     @tasks = Task.all
   end
+
+  def new # Only cares a bout showing the form
+  end
+
+  def create # has access to user data, from the form
+    task = Task.new(title: params[:title] ,description: params[:description], due_date: params[:due_date], status: params[:status])
+    task.save
+    redirect_to('/tasks')
+  end
+
+  # def show
+  # end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
