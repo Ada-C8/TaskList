c01d4705a6a4eb8ff0a67fb2de7c9be32b303aa3
Tanisha <tanishahampden@gmail.com>
2017-09-20 11:21:19 -0700

    add view for new method and add redirect create method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,14 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new #only cares about showing the form
+  end
+
+  def create # has access to user data, from the form
+    task = Task.new(name: params[:name], description: params[:description], completion_date: params[:completion_date])
+    task.save
+
+    redirect_to('/tasks')
+  end
 end
