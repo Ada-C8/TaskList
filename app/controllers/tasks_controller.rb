d4a146ee39173c4c0080599d662b1093a8845383
Amy Cash <amymcash@gmail.com>
2017-09-20 11:08:17 -0700

    Work on adding button while following along in class

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,10 +3,21 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new #only cares about showing the form
+  end
+
   def edit_task
   end
 
-  def create
+  def create #hash access to user data, from the form,
+    a = Task.new(title: params[:title], description: params[:description], date: params[:date])
+    a.save
+    redirect_to('/tasks')
+    # puts params
+    # x = Task.new(title: "Ada" , description: "it's a name")
+    # puts x
+    # x.save
+
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,19 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-# get '/tasks/new', to: 'tasks#new', as: 'new_task'
+get '/tasks/new', to: 'tasks#new', as: 'new_task'
+post '/tasks/new', to: 'tasks#new', as: 'add_task'
+
 get '/tasks', to: 'tasks#index', as: 'index'
-#
-# get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-#
-# post '/tasks', to: 'tasks#create', as: 'create_task'
-# get '/tasks/:id', to: 'tasks#show', as: 'show_task'
-#
-# patch '/tasks:id', to: 'tasks#update', as: 'update_task'
-# delete '/tasks:id', to: 'tasks#destroy', as: 'delete'
+
+
+get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+post '/tasks', to: 'tasks#create', as: 'create_task'
+get '/tasks/:id', to: 'tasks#show', as: 'show_task'
+
+patch '/tasks:id', to: 'tasks#update', as: 'update_task'
+delete '/tasks:id', to: 'tasks#destroy', as: 'delete'
 
 
 
