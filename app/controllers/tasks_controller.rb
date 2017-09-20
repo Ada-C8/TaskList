51309acafa8773ebb558df15592944f136c4946f
Sarah Read-Brown <srbusiness2@gmail.com>
2017-09-20 14:38:41 -0700

    adds form_for method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,9 +5,14 @@ class TasksController < ApplicationController
   end
 
   def new # Only cares a bout showing the form
+    @task = Task.new
   end
 
   def create # has access to user data, from the form
+    #before, using a hand rolled form, the format of the data coming onto the wire
+    # was different so the way we pulled it out was also different
+    #book = Book.new(title: params[:title] ,description: params[:description], due_date: params[:due_date], status: params[:status])
+
     task = Task.new(title: params[:task][:title] ,description: params[:task][:description], due_date: params[:task][:due_date], status: params[:task][:status])
     task.save
     redirect_to('/tasks')
