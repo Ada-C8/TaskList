db6ef4715709e05754a5a5c244661cfece685939
Amy Lee <amy.yj.lee@gmail.com>
2017-09-20 16:08:54 -0700

    Passing Wave 2, beginning CSS styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,38 @@
 class TasksController < ApplicationController
+
   def index
+    @tasks = Task.all.order(:id)
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task= Task.new(name: params[:task][:name], description: params[:task][:description])
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def show
+    @task = Task.find(params[:id].to_i)
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def mark_completion
+    @task= Task.find(params[:id].to_i)
+    @task.status ? @task.update(status: false): @task.update(status: true)
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: 'tasks#index', as: 'root' #root_path
+
   get '/tasks/', to: 'tasks#index', as: 'tasks' #tasks_path
 
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' #edit_task_path
+
+  patch '/tasks/:id/mark_completion', to: 'tasks#mark_completion', as: 'mark_completion_task' #mark_completion_task_path
 
   get '/tasks/new', to: 'tasks#new', as: 'new-task' #new_task_path
 
@@ -9,7 +14,7 @@ Rails.application.routes.draw do
 
   patch '/tasks/:id', to: 'tasks#update', as: 'update_task' #update_task_path
 
-  post '/tasks', to: 'tasks#ceate', as: 'create_task' #create_task_path
+  post '/tasks', to: 'tasks#create', as: 'create_task' #create_task_path
 
   delete '/tasks', to: 'tasks#destroy', as: 'delete_task' #delete_task_path
 
