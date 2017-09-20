16abe78801ff51211504d47ac7e813a11179e834
Tanja Stroble <plusdeltah@gmail.com>
2017-09-19 21:56:55 -0700

    finished wave 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,29 +1,32 @@
 class TasksController < ApplicationController
 
-  TASKS = [
-    {id:1,title: "Finish homework", description:"finish all homework assigned", due:"every day", priority: 1, completed: "no"},
-    {id:2,title: "Prepare lunch", description:"prepare food", due:"every day", priority: 3, completed: "no"},
-    {id:3,title: "Sleep", description:"defrag brain",due:"every day", priority: 2, completed: "no"},
-    {id:4,title: "Sweat", description:"work out", due:"every day",priority: 4, completed: "no"}
-  ]
+  # TASKS = [
+  #   {id:1,title: "Finish homework", description:"finish all homework assigned", due:"every day", priority: 1, completed: "no"},
+  #   {id:2,title: "Prepare lunch", description:"prepare food", due:"every day", priority: 3, completed: "no"},
+  #   {id:3,title: "Sleep", description:"defrag brain",due:"every day", priority: 2, completed: "no"},
+  #   {id:4,title: "Sweat", description:"work out", due:"every day",priority: 4, completed: "no"}
+  # ]
   def index
-    @tasks = TASKS
+    @tasks = Task.order(:name) #sorted by title
   end
 
   def show
-    title = @task = params[:id].to_i
-    @task = nil
-    TASKS.each do |task|
-      if task[:id] = id
-        @task = task
-      end
-    end
+    @task = Task.find(params[:id].to_i)
+    # title = @task = params[:id].to_i
+    # @task = nil
+    # TASKS.each do |task|
+    #   if task[:id] = id
+    #     @task = task
+    #   end
+    # end
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+
   end
 
   def complete
@@ -36,5 +39,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task = Task.find(params[:id]).destroy
   end
 end
