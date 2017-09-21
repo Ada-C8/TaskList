705d2e9fc2a477d0ef2ac69af9c1fa5d8546a4d3
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-09-21 16:34:07 -0700

    complete wave 3 with working edit button

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   end
 
   def new #only cares about showing the form
-
+    @task = Task.new
   end
 
   def create # has access to user data, from the form
@@ -12,19 +12,42 @@ class TasksController < ApplicationController
     task = Task.new(name: params[:task][:name], description: params[:task][:description], due_date: params[:task][:due_date])
     task.save
     redirect_to('/tasks')
+
+    # can also do
+    # task = Task.new
+    # task.name = params[:task][:name]
+    # task.description = params[:task][:description]
+    # book.save
   end
 
   def show
-    @task = Task.find (params[:id].to_i)
+    @task = Task.find (params[:id])
   end
 
-  def edit
+  def edit #i am assuming this will work like new, ehree the update is what matters
+    @task = Task.find (params[:id]) #needs to be instance variable; won't work if local variable
   end
 
   def update
+    task_updates = params[:task]
+
+    # task_updates = name: params[:task][:name], description: params[:task][:description], due_date: params[:task][:due_date])
+    @task = Task.find (params[:id])
+    # @task.update_attributes(task_updates) #don't know how to use this right now
+    @task.name = task_updates[:name]
+    @task.description = task_updates[:description]
+    @task.due_date = task_updates[:due_date]
+    @task.save
+    # redirect_to('/tasks')
+    redirect_to task_path(@task)
   end
 
   def destroy
+    @task = Task.find(params[:id]).destroy
+
+    # @task.destroy
+    redirect_to task_path
   end
+
 
 end
