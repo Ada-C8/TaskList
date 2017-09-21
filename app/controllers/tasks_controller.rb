5e372d35ee33746d2bdf41d2d51a2cd9e68eb93a
Irene DeVera <irenneeyyy@gmail.com>
2017-09-20 20:26:45 -0700

    Add links to each task, updated add task page, and css UI

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,14 +4,23 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
-    # you would want to use Task.save
-    redirect_to('/tasks') # This will redirect to the index page because we are telling where the path should go. This should ultimately direc to the show route
+    # NO INSTANCE METHOD IN A CREATE METHOD
+    # task = Task.new(title: params[:title], author: params[:author])
+    task = Task.new(title: params[:task][:title], description: params[:task][:description], due_date: params[:task][:due_date], status: false)
+    task.save
+    redirect_to('/tasks')
+
+    # This will redirect to the index page because we are telling where the path should go. This should ultimately direc to the show route
   end
 
   def show
+    @task = Task.find(params[:id])
+
+    # Params key [] will match whatever we put in the route so in this case it is id
   end
 
   def edit
