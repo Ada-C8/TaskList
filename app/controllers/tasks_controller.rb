b03e5301468bc9c2e924fe60267491b3a9f3d188
sairagula <abdukhalieva@gmail.com>
2017-09-20 22:51:59 -0700

    Wave2, started adding new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,28 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    # task = Task.new(title: params[task][:title], description: params[task][:description], due_date: params[task][due_date], complete: params[task][complete])
+    # task.save
+    # redirect_to('/tasks')
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  # def edit
+  #   @book = Task.find(params[:id])
+  # end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
