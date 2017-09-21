a5cf03a14eaaf277823b226833494d71b6932901
Tamira Vojnar <tamiravojnar@gmail.com>
2017-09-21 14:21:30 -0700

    Add functional edit and delete links

--- task-list/app/controllers/tasks_controller.rb
+++ task-list/app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
   def index
-
     @tasks = Task.all
     # @tasks = [
     #   {title: "Clean kitchen", discription: "Clean the counters and the stove", due_date: "9/30/17", competion_status: "Not completed"},
@@ -20,6 +19,7 @@ class TasksController < ApplicationController
   def create
     task = Task.new(title: params[:task][:title], description: params[:task][:description], due_date: params[:task][:due_date], completion_status: params[:task][:completion_status])
     task.save
+    # TODO: update to use a path, tasks_path
     redirect_to('/tasks')
   end # create
 
@@ -28,12 +28,25 @@ class TasksController < ApplicationController
   end # edit
 
   def update
+    # TODO: edit code to use a local variable to store the params data. Below is class notes for example:
+    # book_update = params[:book] # this is a hash with all the updated book data
+    #     @book = Book.find(params[:id])
+    #     @book.author = book_update[:author]
+    #     @book.tite = book_update[:title]
+    #     @book.save
     @task = Task.find(params[:id])
     @task.title = params[:task][:title]
     @task.description = params[:task][:description]
     @task.due_date = params[:task][:due_date]
     @task.completion_status = params[:task][:completion_status]
     @task.save
+    # TODO: redirect to show! redirect_to task_path(@task)
     redirect_to('/tasks')
   end #update
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end # destroy
 end
