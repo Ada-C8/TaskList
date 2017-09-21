34e602844125d4c762e146305275e5b2bbceeecd
Mariana <lmarianarp@gmail.com>
2017-09-21 15:35:35 -0700

    wave 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,15 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
-      # {
-      #   title: "Run",
-      #   description: "Runs 2 miles close to the river ",
-      #   duedate: "Wensday",
-      #   complete: "no"
-      # },
-      # {
-      #   title: "Grocery",
-      #   description: "To buy the food for the week",
-      #   duedate: "Monday",
-      #   complete: "no"
-      # },
-      # {
-      #   title: "Homework",
-      #   description: "Make my personal web site",
-      #   duedate: "Monday",
-      #   complete: "yes"
-      # }
-    
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create #Has access to user data, from the form
+    task = Took.new(title: params[:title], description: params[:description], duedate: params[:duedate], complete: params[:complete])
+    task.save
+    redirect_to('/tasks')
   end
 end
