d7c75a496fa2a1e1eee804d7765572e6dd949d7a
Stef <seedsofthesesame@gmail.com>
2017-09-20 19:07:10 -0700

    Tasks table has completed column.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919225752) do
+ActiveRecord::Schema.define(version: 20170921015601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170919225752) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.boolean "completed", default: false
   end
 
 end
