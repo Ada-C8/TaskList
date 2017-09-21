0585aaeeaa2801527a9b6f6748793ceab53d2961
Tamira Vojnar <tamiravojnar@gmail.com>
2017-09-20 17:06:23 -0700

    trying again to save step 1 of wave 2

--- task-list/app/controllers/tasks_controller.rb
+++ task-list/app/controllers/tasks_controller.rb
@@ -8,4 +8,18 @@ class TasksController < ApplicationController
     #   {title: "Order shoes", discription: "Buy new sneakers for fall", due_date: "9/22/17", competion_status: "Completed"}
     # ]
   end # index
+
+  def show
+    @task = Task.find(params[:id])
+  end # show
+
+  def new
+    @task = Task.new
+  end # new
+
+  def create
+    task = Task.new(title: params[:task][:title], description: params[:task][:description], due_date: params[:task][:due_date], completion_status: params[:task][:completion_status])
+    task.save
+    redirect_to('/tasks')
+  end # create
 end
