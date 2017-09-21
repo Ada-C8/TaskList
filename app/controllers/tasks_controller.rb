78d7118425d8b99db3680741a6794c605d4b19ef
Jessica Owens <vertige@gmail.com>
2017-09-20 17:20:49 -0700

    Completed Wave 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,18 +8,27 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def show
-    @task = Task.find(params[:id].to_i)
+    @task = Task.find(params[:id])
   end
 
   def update
   end
 
   def create
+    @task = Task.new(name:params[:task][:name], description:params[:task][:description])
+
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    Task.find(params[:id]).destroy
   end
 end
