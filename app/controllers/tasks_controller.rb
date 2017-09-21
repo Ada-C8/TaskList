d6b0af702a2df68f5006138228c6cf3489e45850
Kayla Ecker <The Email Associated with Your Github Account>
2017-09-21 13:37:48 -0700

    added the update feature to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,10 +28,33 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find_by(id: params[:id].to_i)
+
+    unless @task
+      redirect_to tasks_path
+    end
   end
 
   def update
+    task = Task.find_by(id: params[:id].to_i) #does not need to be instance var
+    redirect_to tasks_path unless task #if couldnt find book, will redirect to home page
+
+    if task.update_attributes task_params
+      redirect_to root_path #or root_path - or books_path or flash notice
+    else
+      render :edit
+    end
+
   end
+
+
+  private def task_params
+    return params.require(:task).permit(:name, :due_date, :description)
+  end
+
+
+
+
 
   def new
     @task = Task.new
