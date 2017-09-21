3cf01aa01027c2dfd8961d25cb51090f2726f930
Andrea Chen <andreachen.ac@gmail.com>
2017-09-21 13:16:48 -0700

    Add edit functionatlity.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,71 @@
+
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all #=> unsorted
+
+    #@tasks = Task.order(:title)
+  end
+
+  def new #only cares about showing the form
+
+    #@book = Book.new => acting as a template to generate a form
+    @task = Task.new
+
+  end
+
+  def create #has access to user data, from the form
+    # create local variables only (no instance variables); you don't want data to be displayed to erb file, you are just redirecting here
+
+    task = Task.new(title: params[:task][:title], description: params[:task][:description], due_date: params[:task][:due_date], status: false)
+
+    task.save
+    redirect_to tasks
+    # redirect_to("/tasks")
+
+    # book = Book.new(title: params[:title], author: params[:title])
+
+    # book = Book.new(title: params[:book][:title], author: params[:book][:title])
+
+    # book = Book.new
+    # book.title = params[:book][:title]
+    # book.author = params[:book][:title])
+
+    # book = Book.new(title: "Hello", author: "World")
+    # book.save
+    # redirect_to('/books')
+
+  end
+
+  def show
+    # must be an instance variable because you will be passing this to HTML to display
+    @task = Task.find( params[:id].to_i )
+  end
+
+  def edit
+    @task = Task.find( params[:id] )
+
+  end
+
+  def update
+    # change :status to true
+    task_updates = params[:task] #get user input
+    @task = Task.find( params[:id] ) #find object that needs updating
+
+    @task.title = task_updates[:title]
+    @task.description = task_updates[:description]
+    @task.due_date = task_updates[:due_date]
+    # @task.update_attributes(task_updates)
+    # @task.save
+
+    @task.save
+    redirect_to task_path(@task)
+
+  end
+
+  def destroy
+    # book.destroy
+    Task.find( params[:id] ).destroy
+    redirect_to tasks
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "books#index"
+
+  #prefixes ONLY for GETs path, not for POST path
+
+  get "/tasks", to: "tasks#index", as: "tasks"
+  get "/tasks/new", to: "tasks#new", as: "new_task"
+  post "/tasks", to: "tasks#create"
+  get "/tasks/:id", to: "tasks#show", as: "task"
+  get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  patch "/tasks/:id", to: "tasks#update"
+  delete "/tasks/:id", to: "tasks#destroy"
+
+  # post "books/Lid/checkout", to: "books#checkout", as: "checkout_book"
+
+end
