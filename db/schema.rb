1e1b6ead9514cd752fbcc4edfd6c861ff7c53459
murog <guillermina.muro@gmail.com>
2017-09-22 13:58:34 -0700

    added priority column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919221910) do
+ActiveRecord::Schema.define(version: 20170922203753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170919221910) do
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "priority", default: 0
   end
 
 end
