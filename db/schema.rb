40c3d63a51f62900b76b9efca65af41a26418dca
enigmagnetic <lauren.cardella@gmail.com>
2017-09-22 13:28:09 -0700

    Remove complete column for tasks, logic uses presence of completion date to determine status.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920074648) do
+ActiveRecord::Schema.define(version: 20170922202520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20170920074648) do
     t.date "completion_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "complete"
   end
 
 end
