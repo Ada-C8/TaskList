87cadfc13ff8deb654623108e56f99e7b378822a
murog <guillermina.muro@gmail.com>
2017-09-22 13:39:36 -0700

    updated styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,17 +30,20 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id].to_i)
   end
 
   def update
-    # id = params[:id].to_i
-    # @task = nil
-    # TASKS.each do |task|
-    #   if task[:id] == id
-    #     @task = task
-    #     @task[:completed]= true
-    #   end
-    # end
+    @task = Task.find(params[:id].to_i)
+    @task.owner = params[:task][:owner]
+    @task.title = params[:task][:title]
+    @task.description = params[:task][:description]
+    @task.deadline =  Date.civil(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i)
+    if @task.save
+      redirect_to '/tasks'
+    else
+      render :new
+    end
   end
 
   def mark_complete
@@ -59,8 +62,9 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(owner: params[:task][:owner], title: params[:task][:title], description: params[:task][:description], deadline: Date.civil(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i))
+    task = Task.new(owner: params[:task][:owner], title: params[:task][:title], description: params[:task][:description], deadline: Date.civil(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i), completed: false)
     if task.save
+      flash[:notice] = "#{task.title} by #{task.owner} was successfully created"
       redirect_to '/tasks'
     else
       render :new
@@ -69,5 +73,15 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task = Task.find(params[:id].to_i)
+    owner = @task.owner
+    title = @task.title
+    completed = @task.completed
+    if @task.destroy
+      completed ? (flash[:notice] = "#{title} was successfully deleted, great job #{owner}!") : (flash[:notice] = "#{title} was successfully deleted, better luck next time #{owner}!")
+      redirect_to '/tasks'
+    else
+      render :new
+    end
   end
 end
---
+++ db/migrate/20170922203753_add_priority.rb
@@ -0,0 +1,4 @@
+class AddPriority < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
