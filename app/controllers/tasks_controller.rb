f3d7254b4bed5da642bd732b04a3ff75a01374fd
Sarah Read-Brown <srbusiness2@gmail.com>
2017-09-21 17:10:13 -0700

    Adds a function edit and update logic

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,14 +19,24 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task = Task.find_by(params[:id])
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @task = Task.find_by(params[:id])
   end
 
   def update
+    task_updates = params[:task] #gets user input
+    @task = Task.find(params[:id]) #find the task object that needs to be updated
+
+    @task.title = task_updates[:title]
+    @task.description = task_updates[:description]
+    @task.due_date = task_updates[:due_date]
+    @task.status = task_updates[:status]
+    @task.save
+
+    redirect_to task_path(@task)
   end
 
   def destroy
