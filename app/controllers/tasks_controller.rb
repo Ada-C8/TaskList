7a2a8999025cd5d62a3af2357e731dc02eec7e4b
enigmagnetic <lauren.cardella@gmail.com>
2017-09-21 17:29:46 -0700

    Build edit view with form partial, add update, edit, and private params methods to controller.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,10 +12,15 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find_by(id: params[:id].to_i)
+
+    unless @task
+      redirect_to task_path
+    end
   end
 
   def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description], due_date: params[:task][:due_date])
+    @task = Task.new(task_params)
 
     if @task.save
       redirect_to root_path
@@ -25,8 +30,21 @@ class TasksController < ApplicationController
   end
 
   def update
+    task = Task.find_by(id: params[:id].to_i)
+    redirect_to tasks_path unless task
+
+    if task.update_attributes task_params
+      redirect_to tasks_path
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
+
+  private
+    def task_params
+      return params.require(:task).permit(:name, :description, :due_date, :completion_date, :completed)
+    end
 end
