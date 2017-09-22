e2b04600b1ae96247dd173b7d73433806be558d8
Maria McGrew <mcgmar1@outlook.com>
2017-09-22 10:58:43 -0700

    edit action working and redirecting to show page after update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,9 +16,21 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find_by(id: params[:id].to_i)
+    unless @task
+      redirect_to task_path
+    end
   end
 
   def update
+    task = Task.find_by(id: params[:id].to_i)
+    redirect_to tasks_path unless task #this redirects to the books_path if the book can't be found
+
+      if task.update_attributes task_params
+        redirect_to task_path
+      else
+        render :edit
+      end
   end
 
   def new
@@ -36,5 +48,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+   return params.require(:task).permit(:name, :description, :completion_date)
   end
 end
