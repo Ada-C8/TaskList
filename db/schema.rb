9e450d21d70a38979d62f73dc2e6d41096dc8a89
Sarah Read-Brown <srbusiness2@gmail.com>
2017-09-22 10:28:49 -0700

    Data base migration to change the name and data type of status column to complete?

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919221640) do
+ActiveRecord::Schema.define(version: 20170922171411) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.string "due_date"
-    t.string "status"
+    t.boolean "complete?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
