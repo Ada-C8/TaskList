f499e91e92b0b9ae6046aa304facd9bdf6ec2da2
Isaac Del Rosario <cecdelr@gmail.com>
2017-09-22 14:02:04 -0700

    Implemented edit and delete functions, created partials for forms

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   post '/tasks', to: 'tasks#create', as: 'create_task'
 
-  delete '/tasks', to: 'tasks#delete', as: 'delete_task'
+  delete '/tasks/:id', to: 'tasks#delete', as: 'delete_task'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
