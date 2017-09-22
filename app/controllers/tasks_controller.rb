4cd75db65fe2a913e7451cf898756694eb9f4e71
Gale Harrington <gale.harrington19@gmail.com>
2017-09-22 10:43:57 -0700

    Commit for the first time - oops

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,56 @@
 class TasksController < ApplicationController
-  TASKS = [{task: "Call Mom"}, {task: "Laundry"}, {task: "Groceries"}]
-
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   def add
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find( params[:id].to_i )
   end
 
   def remove
+    Task.find_by(id: params[:id]).destroy
+
+    redirect_to root_path
   end
 
   def edit
+    @task = Task.find_by(id: params[:id].to_i)
+    unless @task
+      redirect_to root_path
+    end
+  end
+
+  def update
+    task = Task.find_by(id: params[:id].to_i)
+    redirect_to root_path unless task
+
+    task.name = params[:task][:name]
+    task.description = params[:task][:description]
+    task.date = params[:task][:date]
+
+    if task.update(name: task.name, description: task.description, date: task.date)
+      redirect_to task_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+    if @task.save
+      redirect_to root_path
+    else
+      render :add
+    end
+  end
+
+  private
+
+  def task_params
+    return params.require(task).permit(:name, :description, :date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+
   get 'tasks', to: 'tasks#index', as: 'tasks'
 
   get 'tasks/add', to:'tasks#add', as: 'add_task'
 
-  get 'tasks/remove'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
 
-  get 'tasks/edit'
+  patch 'tasks/:id', to: 'tasks#update', as: 'update_task'
+
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  get 'tasks/:id/remove', to: 'tasks#remove', as: 'remove_task'
+
+  post 'tasks/', to: 'tasks#create', as: 'create_task'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
