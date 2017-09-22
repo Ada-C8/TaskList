3f374c7f7237c5a40fbc063455d09c7dcc00d833
Canaan West <canaan.west@gmail.com>
2017-09-22 15:54:17 -0700

    forgot to commit oops

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,35 +1,71 @@
 class TasksController < ApplicationController
   #write a list of tasks in a hash
-  TASKS =[{id: 1, thing:"Go for a walk"}, {id: 2, thing:"groceries"}, {id: 3, thing:"Do this task builder project"}, {id: 4, thing:"lightening talk"}]
+  # TASKS =[{id: 1, thing:"Go for a walk", status: "incomplete"}, {id: 2, thing:"groceries", status: "incomplete"}, {id: 3, thing:"Do this task builder project", status: "incomplete"}, {id: 4, thing:"lightening talk", status: "incomplete"}]
 
   #write an index function that returns
 
   def index
-    @tasks = TASKS
+    @tasks = Task.all.order(:id)
   end
 
   def show
-    # id = params[:id].to_i
-    # @task = nil
-    # TASKS.each do |task|
-    #   if task[:id] == id
-    #     @task = task
-    #   end
-    # end
+    @task = Task.find(params[:id].to_i)
   end
 
   def create
+    @task = Task.new(name: params[:task][:name], description:params[:task][:description], due:params[:task][:due], status: "incomplete")
+    @task.save
+
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def update
+    task = Task.find_by(id: params[:id].to_i)
+
+    redirect_to tasks_path unless task
+
+    if task.update_attributes task_params
+      redirect_to task_path(task.id)
+    else
+      render :edit
+    end
+
   end
 
   def destroy
+    @task = Task.find_by(id: params[:id].to_i)
+    @task.destroy
+    redirect_to root_path
   end
 
   def edit
+    @task = Task.find_by(id: params[:id].to_i)
   end
 
   def new
+    @task = Task.new
   end
+
+  def mark_unmark
+    @task = Task.find(params[:id].to_i)
+    if @task.status == "incomplete"
+      @task.status = "complete"
+      @task.completion_date = DateTime.now
+    else
+      @task.status = "incomplete"
+      @task.completion_date = nil;
+    end
+    @task.save
+    redirect_to tasks_path
+  end
+end
+
+private
+
+def task_params
+  return params.require(:task).permit(:name, :description, :due)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+
   get '/tasks', to: 'tasks#index' , as:'tasks' #tasks_path
 
   get 'tasks/:id/edit', to: "tasks#edit" , as: 'edit_task' #edit_task_path
 
-  get 'tasks/new', to: 'tasks#new', as: 'new_book' #new_book_path
+  get 'tasks/new', to: 'tasks#new', as: 'new_task' #new_book_path
 
   get 'tasks/:id', to: 'tasks#show', as: 'task' #task_path
 
-  patch 'tasks/:id', to: "task#update" , as: 'update_task' #update_task_path
+  patch 'tasks/:id/mark_unmark', to: 'tasks#mark_unmark', as: 'mark_unmark_status' #mark_unmark_status_path
 
-  post 'tasks/create', to: 'tasks#create', as: 'create_task' #create_task_path
+  patch 'tasks/:id', to: "tasks#update" , as: 'update_task' #update_task_path
 
-  delete '/tasks', to: 'tasks#destroy', as: 'delete_task' #delete_task_path
+  post '/tasks', to: 'tasks#create', as: 'create_task' #create_task_path
+
+  delete 'tasks/:id/destroy', to: 'tasks#destroy', as: 'delete_task' #delete_task_path
 
 
 
