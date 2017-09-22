db4fe666741ede1b60eaaac755fc6595f8686ff4
Tanisha <tanishahampden@gmail.com>
2017-09-22 10:01:35 -0700

    update destroy action to use destroy method instead of delete method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,7 @@ class TasksController < ApplicationController
   end
 
   def new #only cares about showing the form
+    @task = Task.new
   end
 
   def create # has access to user data, from the form
@@ -19,6 +20,36 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def edit
+    # will throw error if no record
+    @task = Task.find(params[:id])
+
+    # # will return nil if no record,
+    # @task = Task.find_by(:id)
+    #
+    # # will return empty array if no record
+    # @task = Task.where(id)
+  end
+
+  def update
+    task_updates = params[:task]
+    @task = Task.find(params[:id])
+    # @task.update_attributes(task_updates)
+    # @task.save
+
+    @task.name = task_updates[:name]
+    @task.description = task_updates[:description]
+    @task.completion_date = task_updates[:completion_date]
+    @task.save
+
+    redirect_to task_path(@task)
+
+  end
+
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
   end
 end
