e96b9e621897f2024a1c71fa2c75bf7020118dd1
Tanja Stroble <plusdeltah@gmail.com>
2017-09-22 10:51:19 -0700

    added a popup to confirm delete in show view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get '/tasks', to: 'tasks#index', as: 'tasks' #tasks_path
 
+  get '/task/:id/confirm', to: 'tasks#confirm', as: 'confirm_task' # confirm_task_path
+
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' #edit_task_path
 
   get 'tasks/new', to: 'tasks#new', as: 'new_task' #new_task_path
@@ -13,8 +15,6 @@ Rails.application.routes.draw do
   patch '/tasks/:id', to:'tasks#update', as: 'update_task' #update_task_path
 
   post '/tasks', to:'tasks#create', as: 'create_task' #create_task_path
-
-  # patch '/task/:id/complete', to: 'tasks#complete', as: 'complete_task' #complete_task_path
 
   delete '/tasks/:id', to: 'tasks#destroy', as: 'delete_task' #delete_task_path
 
