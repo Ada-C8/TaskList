cca3519c116550704965cdc9f878225df05cb905
Rebecca Bergena <rbergena@gmail.com>
2017-09-22 09:49:26 -0700

    add delete route

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,43 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date], complete: params[:task][:complete] = false)
+    task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find_by(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    task_updates = params.require(:task).permit(:name, :description, :completion_date, :complete)
+    @task.update_attributes(task_updates)
+    @task.save
+
+    if params[:refresh]
+      redirect_to tasks_path
+    else
+      redirect_to task_path(@task)
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+end
---
+++ db/migrate/20170919220649_change_completed_date_data_type_to_date.rb
@@ -0,0 +1,5 @@
+class ChangeCompletedDateDataTypeToDate < ActiveRecord::Migration[5.1]
+  def change
+    change_column(:tasks, :completion_date, :date)
+  end
+end
