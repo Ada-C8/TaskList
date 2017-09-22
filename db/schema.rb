8a30611af242caa8d6e94dab1006f99036b515e0
Gale Harrington <gale.harrington19@gmail.com>
2017-09-22 11:30:40 -0700

    Add completion_date to DB, add checkboxes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919222950) do
+ActiveRecord::Schema.define(version: 20170922182050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170919222950) do
     t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "completion_date"
   end
 
 end
