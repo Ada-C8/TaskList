866600ff858e4d829ab712668a0f895762202ff2
Mariana <lmarianarp@gmail.com>
2017-09-23 15:39:30 -0700

    Wave 4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,8 +8,35 @@ class TasksController < ApplicationController
   end
 
   def create #Has access to user data, from the form
-    task = Took.new(title: params[:title], description: params[:description], duedate: params[:duedate], complete: params[:complete])
-    task.save
+    @task = Task.create(title: params[:task][:title], description: params[:task][:description], duedate: params[:task][:duedate], complete: false)
+    # task.save
     redirect_to('/tasks')
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(title: params[:task][:title], description: params[:task][:description], duedate: params[:task][:duedate])
+    redirect_to(task_path)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to(tasks_path)
+  end
+
+  def mark
+    @task = Task.find(params[:id])
+    @task.update(complete: params[:task][:complete])
+    redirect_to('/tasks')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   post '/tasks', to: 'tasks#create'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  put '/tasks/:id', to: 'tasks#update'
+  patch '/tasks/:id', to: 'tasks#update'
   delete '/tasks/:id', to: 'tasks#destroy'
   post '/tasks/:id/checkout', to: 'tasks#checkout', as: 'checkout_task'
+  patch '/tasks/:id/mark', to: 'tasks#mark', as:'mark_task' 
 end
