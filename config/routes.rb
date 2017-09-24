c1cf59adb106174ea80d5bac135b39b72e7df3c3
Diane Lenertz <lenertzd@grinnell.edu>
2017-09-24 14:54:13 -0700

    finish baseline

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get('/tasks/new', to: 'tasks#new', as: 'new_task')
   get('/tasks/:id', to: 'tasks#show', as: 'task')
   post('/tasks', to: 'tasks#create')
-  delete('/tasks/:id', to: 'tasks#destroy') # as: 'delete_task')
-
+  get('/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task')
+  patch('/tasks/:id', to: 'tasks#complete', as: 'mark')
+  patch('/tasks/:id', to: 'tasks#update')
+  delete('/tasks/:id', to: 'tasks#destroy')
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920162526) do
+ActiveRecord::Schema.define(version: 20170922214039) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "task"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170920162526) do
     t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "completion_date"
   end
 
 end
