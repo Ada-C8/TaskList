db06e936471e60632352df68ace995d1889fbf22
nkiru onwuneme <generalhrh@gmail.com>
2017-09-26 21:31:17 -0700

    Added the create action to make a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,17 @@ class TasksController < ApplicationController
   def new
      @task = Task.new #creating a new object; allows  rails to retrieve all the fields in that model
   end
+
+  def create
+    @task = Task.new(
+      title: params[:task][:title],
+      description: params[:task][:description],
+      due_date: params[:task][:due_date]
+    )
+      if @task.save
+        redirect_to tasks_path
+      else
+        render :new
+      end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/tasks/new", to: "tasks#new"
-  
+  post "/tasks", to:"tasks#create"
 
   get "/tasks", to: "tasks#index"
   get "/tasks/:id", to: "tasks#show", as: "task"
