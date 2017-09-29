cdd46f848b00e29f9bf51408f530f38c0af5336d
nkiru onwuneme <generalhrh@gmail.com>
2017-09-29 15:04:09 -0700

    Modified css, views and added new column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,55 +1,47 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.all #generate all tasks
+  end
+
+  def new
+    @task = Task.new   #create atemplate of task forrails to use
+  end
+
+  def create
+    task = Task.new(title: params[:task][:title], description: params[:task][:description], due_date: params[:task][:due_date], complete: false)
+    puts params
+    task.save
+
+    redirect_to home_url
+  end
+
+  def edit
+    @task = Task.find(params[:id]) #find a task based on the id requested
+  end
+
+  def update
+    task_updates = params.require(:task).permit(:title, :description, :due_date, :complete, :completed_date) # only looks at what's required i.e what's passed
+    @task = Task.find(params[:id])
+    @task.update_attributes(task_updates)
+    @task.save
+
+    if params[:refresh]
+      redirect_to home_url
+    else
+      redirect_to task_path(@task) #redirect to show
+    end
   end
 
   def show
     @task = Task.find(params[:id])
   end
 
-  def new
-     @task = Task.new #creating a new object; allows  rails to retrieve all the fields in that model
-  end
-
-  def create
-    @task = Task.new(
-      title: params[:task][:title],
-      description: params[:task][:description],
-      due_date: params[:task][:due_date]
-    )
-      if @task.save
-        redirect_to tasks_path
-      else
-        render :new
-      end
-  end
-
-  def edit
+  def destroy
     @task = Task.find(params[:id])
+
+    @task.destroy
+
+    redirect_to home_url
   end
 
-  def update
-    @task = Task.find(params[:id])
-    result = @task.update({
-      title: params[:task][:title],
-      description: params[:task][:description],
-      due_date: params[:task][:due_date]
-      })
-
-      if result
-        redirect_to task_path(@task.id)
-      else
-        render :edit
-      end
-  end
-
-  def delete
-    task = Task.find(params[:id])
-
-      if task.destroy
-        redirect_to tasks_path
-      else
-
-      end
-  end
 end
