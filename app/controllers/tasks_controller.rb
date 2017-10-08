4dc22ad3aaab22457df1867e33a7435f83449118
Sarah Read-Brown <srbusiness2@gmail.com>
2017-10-08 11:37:39 -0700

    Made changes based on Charles' feedback

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
 
     task = Task.new(title: params[:task][:title] ,description: params[:task][:description], due_date: params[:task][:due_date], complete: params[:task][:complete])
     task.save
-    redirect_to('/tasks')
+    redirect_to tasks_path
   end
 
   def show
