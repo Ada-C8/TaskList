701df22565542fbec96180673e41412551b6c997
VirtualLindsey <tapircode@gmail.com>
2017-10-09 17:37:17 -0700

    pushing migration and task

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2017101736) do
+
   create_table "tasks", force: :cascade do |t|
-      t.string "title"
-      t.string "description"
-      t.date "due_date"
-      t.boolean "complete"
-    end
+    t.string "title"
+    t.string "description"
+    t.string "due_date"
+    t.string "complete"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
